fc2109e25a2388cebff6d050cc4ef5911c384bcf
JellicleYang <31367086+JellicleYang@users.noreply.github.com>
2020-03-19 18:05:56 +0800

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -6,10 +6,10 @@ def data(train_data, valid_data, batch_size, workers):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
-    train_transforms = transforms.Compose([transforms.RandomResizedCrop(224),		#对图片尺寸做一个缩放切割
-                                           transforms.RandomHorizontalFlip(),		#水平翻转
-                                           transforms.ToTensor(),					#转化为张量
-                                           normalize	                            #进行归一化
+    train_transforms = transforms.Compose([transforms.RandomResizedCrop(224),		
+                                           transforms.RandomHorizontalFlip(),		
+                                           transforms.ToTensor(),					
+                                           normalize	                            
     ])
 
     val_transforms = transforms.Compose([transforms.Resize(256),
